af162bc8e3c55add9d973f2652f6833535ab66d5
unknown <Multi@MultiM.starwars.dess-gla.infop6.jussieu.fr>
2013-11-27 22:43:41 +0100

    Partial add of funciton recognation

--- sash-3.7/eval-upmc.c
+++ sash-3.7/eval-upmc.c
@@ -1,11 +1,14 @@
 #define SIZE 50            /* Size of Stack */
-#include<stdlib.h>
-#include<string.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #include "eval-upmc.h"
+#include "../AST/Factory.h"
+#include "Function.h"
+
 char s[SIZE];
 int top=-1;       /* Global declarations */
- 
+
 char *strrev(char *str)
 {
       char *p1, *p2;
@@ -20,17 +23,17 @@ char *strrev(char *str)
       }
       return str;
 }
- 
+
 void push(char elem)
 {                       /* Function for PUSH operation */
     s[++top]=elem;
 }
- 
+
 char pop()
 {                      /* Function for POP operation */
     return(s[top--]);
 }
- 
+
 int pr(char elem)
 {                 /* Function for precedence */
     switch(elem)
@@ -42,10 +45,10 @@ int pr(char elem)
     case '*':
     case '/': return 3;
     }
-    
+
     return -1;
 }
- 
+
 char* infixToPrefix(char* infx)
 {
     char* prfx=(char*)malloc(2*strlen(infx)*sizeof(char));
@@ -81,4 +84,59 @@ char* infixToPrefix(char* infx)
     return strrev(prfx);
 }
 
+void functionRecognition(char* str)
+{
+    int isIfThenElse = 0;
+    char* finArg = strchr(str, ')');
+    char* arg = strchr(str, '(');
+    int taille = arg-str;
+
+    char* dest = calloc(taille + 1);
+    strncat(dest, str, taille);
+    dest[taille] = '\0';
+
+    Function *f = Factory::createNewFunction(str, dest);
+    free(dest);
+
+    taille = finArg-arg;
+    dest = calloc(taille + 1);
+    strncat(dest, arg, taille);
+    dest[taille] = '\0';
+
+    std::list<std::string> *l = new std::list<std::string>();
+    l->push_back(Factory::createNewVariable(dest));
+    f->setArgsNames(l);
+
+    f->setExpression(functionRecognitionRec(str, f));
+}
+
+Expression *functionRecognitionRec(char *str, Expression *parent)
+{
+    while (str[0] == ' ')// On retire les premiers blancs
+        str++;
+
+    token = strtok(str, " ");
+    if (strcmp(token, "if") == 0)
+    {
+        token = strtok(NULL, " ");
+        IfThenElse *ifExpr = (IfThenElse*)Factory::createNewIf("");
+
+        str = strpbrk(str, "if");
+        ifExpr->setBoolCondition(functionRecognitionRec(str, ifExpr));
+        str = strpbrk(str, "then");
+        ifExpr->setLeft(functionRecognitionRec(str, ifExpr));
+        str = strpbrk(str, "else");
+        ifExpr->setRight(functionRecognitionRec(str, ifExpr));
+
+        return ifExpr;
+    }
+    else if (strcmp(token, "then") == 0)
+        return NULL;
+    else if (strcmp(token, "else") == 0)
+        return NULL;
+    else
+    {
+        // use arithmetic recognation to constrcut AST
+    }
+}
 
